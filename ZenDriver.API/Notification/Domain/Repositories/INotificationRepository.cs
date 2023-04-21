6f8c78578defb8d2e7c598eaa858e441ebd6c931
ludwin729 <89177248+LudwinReyes@users.noreply.github.com>
2023-04-21 14:19:18 -0500

    feat : added Notification bounded context

---
+++ ZenDriver.API/Notification/Domain/Repositories/INotificationRepository.cs
@@ -0,0 +1,12 @@
+﻿using ZenDriver.API.Notification.Domain.Models;
+
+namespace ZenDriver.API.Notification.Domain.Repositories;
+
+public interface INotificationRepository
+{
+    Task<IEnumerable<Models.Notification>> GetNotificationsAsync();
+    Task AddNotificationAsync(Models.Notification notification);
+    Task<Models.Notification> FindNotificationByIdAsync(int notificationId);    
+    void Remove(Models.Notification notification);
+
+}
---
+++ ZenDriver.API/Notification/Domain/Services/INotificationService.cs
@@ -0,0 +1,12 @@
+﻿using ZenDriver.API.Notification.Domain.Models;
+using ZenDriver.API.Notification.Domain.Services.Communication;
+
+namespace ZenDriver.API.Notification.Domain.Services;
+
+public interface INotificationService
+{
+    Task<IEnumerable<Models.Notification>> GetNotificationsAsync();
+    Task<NotificationResponse> AddNotificationAsync(Models.Notification notification);    
+    Task<NotificationResponse> DeleteAsync(int id);
+
+}
